5da535180bf877394ec44c59e0be105a5649da03
gokul3710 <gokulkrishnawaryar@gmail.com>
2024-03-13 04:57:52 -0400

    added wildcard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
-  {path: "auth", component: AuthComponent, loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+  {path: "", redirectTo: 'auth', pathMatch:"full" },
+  {path: "auth", component: AuthComponent, loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  {path: "**", redirectTo: "auth"}
 ];
 
 @NgModule({
